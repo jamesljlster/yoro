f54cf95a01765770b9e5861c3f51fa5de405e84e
Zheng-Ling Lai <jamesljlster@gmail.com>
2020-05-01 15:20:58 +0800

    Test dictionary info summarize

--- test/test_info_summarize.py
+++ test/test_info_summarize.py
@@ -6,11 +6,13 @@ if __name__ == '__main__':
     info = None
     info1 = {
         'loss': (torch.FloatTensor([0.7]), 1),
-        'obj': ([0.8, 0.2], 4)
+        'obj': ([0.8, 0.2], 4),
+        'accu': ({'class': 0.4, 'deg': 0.6}, 5)
     }
     info2 = {
         'loss': (torch.FloatTensor([0.3]), 1),
-        'obj': ([0.2, 0.8], 4)
+        'obj': ([0.2, 0.8], 4),
+        'accu': ({'class': 0.6, 'deg': 0.4}, 5)
     }
 
     info = info_add(info, info1)

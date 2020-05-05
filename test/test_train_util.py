77c3f99d3a7491ad2e02e3e940f8c649d4a52f2d
Zheng-Ling Lai <jamesljlster@gmail.com>
2020-05-05 22:50:34 +0800

    Update train_util test

--- test/test_train_util.py
+++ test/test_train_util.py
@@ -3,5 +3,6 @@ from yoro.utils.train_util import YOROTrain
 if __name__ == '__main__':
 
     tc = YOROTrain('config/example.yaml')
-    tc.restore()
+    tc.valid()
     tc.train()
+    tc.export_model()

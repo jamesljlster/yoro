ea41b478966d64909ac57b19a6e52d5486663b90
Zheng-Ling Lai <jamesljlster@gmail.com>
2020-05-12 17:21:08 +0800

    Test model forwarding in c++

--- test/test_recall_image.cpp
+++ test/test_recall_image.cpp
@@ -2,10 +2,21 @@
 #include <exception>
 #include <iostream>
 #include <memory>
+#include <vector>
 
 #include <torch/script.h>
+#include <opencv2/opencv.hpp>
+
+#define NET_SIZE 224
 
 using namespace std;
+using namespace cv;
+
+using torch::from_blob;
+using torch::ScalarType;
+using torch::Tensor;
+using torch::jit::load;
+using torch::jit::script::Module;
 
 int main(int argc, char* argv[])
 try
@@ -18,7 +29,41 @@ try
     }
 
     // Import model
-    torch::jit::script::Module model = torch::jit::load(argv[1]);
+    Module model = load(argv[1]);
+    model.eval();
+
+    // Read image
+    Mat src = imread(argv[2], IMREAD_COLOR);
+    cvtColor(src, src, COLOR_BGR2RGB);
+
+    // Pad to square
+    int tarSize = max(src.rows, src.cols);
+    Mat mat = Mat(tarSize, tarSize, CV_8UC3, Scalar(0, 0, 0));
+    Rect roi = Rect((tarSize - src.cols) / 2, (tarSize - src.rows) / 2,
+                    src.cols, src.rows);
+    src.copyTo(mat(roi));
+
+    // Resizing
+    resize(mat, mat, Size(NET_SIZE, NET_SIZE));
+
+    // Convert image to tensor
+    Tensor inputs =
+        from_blob(mat.ptr<char>(), {1, NET_SIZE, NET_SIZE, 3}, ScalarType::Byte)
+            .to(torch::kFloat)
+            .permute({0, 3, 1, 2})
+            .contiguous() /
+        255.0;
+
+    // Forward
+    auto outputs = model.forward({inputs}).toTuple();
+    auto listRef = outputs->elements();
+    Tensor predConf = listRef[0].toTensor();
+    Tensor predClass = listRef[1].toTensor();
+    Tensor predClassConf = listRef[2].toTensor();
+    Tensor predBox = listRef[3].toTensor();
+    Tensor predDeg = listRef[4].toTensor();
+
+    cout << (predConf > 0.9).sum() << endl;
 
     return 0;
 }

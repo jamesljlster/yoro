c307ed2ef74f97e8176dba47a41922d8d94781f8
Cheng-Ling Lai <jamesljlster@gmail.com>
2020-12-06 22:38:04 +0800

    Update pretrain_exporter with latest API

--- utils/pretrain_exporter.py
+++ utils/pretrain_exporter.py
@@ -9,10 +9,15 @@ if __name__ == '__main__':
 
     # Parse arguments
     argp = argparse.ArgumentParser(
-        description='Export pretrain weight from existing model or backup file')
+        description='Export pretrained weight from existing model or backup file')
 
     argp.add_argument('source', type=str, help='Target model or backup file')
     argp.add_argument('destination', type=str, help='Output path')
+
+    opt = argp.add_argument_group('Optional arguments')
+    opt.add_argument('--best', type=bool, default=True,
+                     help=('Export pretrained weight with best model state. '
+                           '(default: %(default)s)'))
 
     args = argp.parse_args()
 
@@ -22,8 +27,11 @@ if __name__ == '__main__':
     if srcExt == '.zip':
         sDict = torch.jit.load(src).backbone.state_dict()
     else:
-        sDict = torch.load(
-            src, map_location=torch.device('cpu'))['bbone_state_dict']
+        bak = torch.load(src, map_location=torch.device('cpu'))
+        if args.best:
+            sDict = bak['best_state']['backbone']
+        else:
+            sDict = bak['model_state_dict']['backbone']
 
     # Save pretrain weight
     torch.save({'bbone_state_dict': sDict}, args.destination)

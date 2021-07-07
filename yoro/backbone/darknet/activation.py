bbbdb6491fa4f09f836461f780936bafb986245a
Cheng-Ling Lai <jamesljlster@gmail.com>
2021-07-07 21:27:32 +0800

    Replace 'linear' activation implementation with builtin operator

--- yoro/backbone/darknet/activation.py
+++ yoro/backbone/darknet/activation.py
@@ -1,22 +1,14 @@
 import torch
-from torch.nn import Module, LeakyReLU
+from torch.nn import Module, LeakyReLU, Identity
 from torch.nn import functional as F
 
 
 # Alias
 leaky = LeakyReLU
+linear = Identity
 
 
 # Implementations
-class linear(Module):
-
-    def __init__(self):
-        super().__init__()
-
-    def forward(self, x):
-        return x
-
-
 class mish(Module):
 
     def __init__(self):

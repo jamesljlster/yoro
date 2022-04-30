20f8e47dc0ff1ef5a4e899fff12d7e2fbce608ab
Cheng-Ling Lai <jamesljlster@gmail.com>
2022-04-30 23:42:46 +0800

    Remove OpenCV dependency for inference api

--- yoro/api/yoro_api_pym.cxx
+++ yoro/api/yoro_api_pym.cxx
@@ -1,9 +1,51 @@
+#include <stdexcept>
+#include <tuple>
+#include <vector>
 
-#include "yoro_api_pym.hpp"
+#include <pybind11/numpy.h>
+
 #include "calc_ops.hpp"
 #include "yoro_api.hpp"
+#include "yoro_api_pym.hpp"
 
 using namespace yoro_api;
+using namespace pybind11::detail;
+
+std::tuple<const uint8_t*, int, int, int> array_to_image(
+    const py::array& buffer)
+{
+    // Check if input array is contiguous
+    if (!(buffer.flags() & npy_api::constants::NPY_ARRAY_C_CONTIGUOUS_))
+    {
+        throw std::invalid_argument(
+            "Input array should be C-style contiguous!");
+    }
+
+    // Check array dtype
+    py::dtype bufType = buffer.dtype();
+    if (!(bufType.is(py::dtype::of<unsigned char>()) ||
+          bufType.is(py::dtype::of<signed char>())))
+    {
+        throw std::invalid_argument(
+            "Dtype of input array should be either np.uint8 or np.int8");
+    }
+
+    // Check array dimensions
+    int ndim = buffer.ndim();
+    std::vector<int> sizes({0, 0, 1});
+    if (ndim < 2 || ndim > 3)
+    {
+        throw std::invalid_argument(
+            "Dimensions of input array should be 2 or 3!");
+    }
+
+    for (int i = 0; i < ndim; i++)
+    {
+        sizes[i] = buffer.shape(i);
+    }
+
+    return {(const uint8_t*)buffer.data(), sizes[0], sizes[1], sizes[2]};
+}
 
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m)
 {
@@ -42,23 +84,39 @@ PYBIND11_MODULE(TORCH_EXTENSION_NAME, m)
     py::class_<YORODetector>(m, "YORODetector")
         .def(py::init<const std::string&, const DeviceType&>())
         .def(py::init<const std::string&>())
-#ifdef WITH_OPENCV
         .def(
             "detect",
-            py::overload_cast<const cv::Mat&, float, float>(
-                &YORODetector::detect))
-#endif
-        ;
+            [](YORODetector& detector,
+               const py::array& image,
+               float confTh,
+               float nmsTh)
+            {
+                std::tuple<const uint8_t*, int, int, int> data =
+                    array_to_image(image);
+                return detector.detect(
+                    std::get<0>(data),
+                    std::get<1>(data),
+                    std::get<2>(data),
+                    std::get<3>(data),
+                    confTh,
+                    nmsTh);
+            });
 
     py::class_<RotationDetector>(m, "RotationDetector")
         .def(py::init<const std::string&, const DeviceType&>())
         .def(py::init<const std::string&>())
-#ifdef WITH_OPENCV
         .def(
             "detect",
-            py::overload_cast<const cv::Mat&>(&RotationDetector::detect))
-#endif
-        ;
+            [](RotationDetector& detector, const py::array& image)
+            {
+                std::tuple<const uint8_t*, int, int, int> data =
+                    array_to_image(image);
+                return detector.detect(
+                    std::get<0>(data),
+                    std::get<1>(data),
+                    std::get<2>(data),
+                    std::get<3>(data));
+            });
 
     m.def("bbox_to_corners", bbox_to_corners);
     m.def("rbox_similarity", rbox_similarity);
